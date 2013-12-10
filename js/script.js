03fec96f57b602ae1426063ffbaa78b4b60977ab
Sundus Asif <sundus_06@hotmail.com>
2013-12-10 11:35:34 -0500

    FInal Fixes

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $(window).bind('resize orientationchange', function() {
 });
 
 var adjustMenu = function() {
-	if (ww < 600) {
+	if (ww < 635) {
 		$(".toggleMenu").css("display", "inline-block");
 		if (!$(".toggleMenu").hasClass("active")) {
 			$(".nav").hide();
@@ -36,7 +36,7 @@ var adjustMenu = function() {
 			$(this).parent("li").toggleClass("hover");
 		});
 	} 
-	else if (ww >= 600) {
+	else if (ww >= 635) {
 		$(".toggleMenu").css("display", "none");
 		$(".nav").show();
 		$(".nav li").removeClass("hover");
